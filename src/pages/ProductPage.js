4a8ca6d2f1be2b99155c25d7c2c29199cf1b5ec9
mohamed Abdel Fatah <91346253+moahmeda-abdo@users.noreply.github.com>
2023-10-23 16:31:47 +0300

    Update ProductPage.js

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -117,7 +117,7 @@ export default function ProductPage() {
   const addToCartHandler = async () => {
     const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
+    const { data } = await axios.get(`https://server-e-commerce-txaw.onrender.com/api/products/${product._id}`);
     if (data.countInStock < quantity) {
       window.alert("Sorry. Product is out of stock");
       return;
